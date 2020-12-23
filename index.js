53da2adb2874699089079704b4dd45fa9436ba88
ZhengjieYe <20091571@mail.wit.ie>
2020-12-23 21:33:13 +0000

    Web-config: initial project

---
+++ index.js
@@ -0,0 +1,54 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import moviesRouter from './api/movies';
+import bodyParser from 'body-parser';
+import './db';
+import {loadUsers, loadMovies} from './seedData';
+import usersRouter from './api/users';
+import genresRouter from './api/genres'
+import session from 'express-session';
+import passport from './authenticate';
+
+dotenv.config();
+
+if (process.env.SEED_DB) {
+  loadUsers();
+  loadMovies();
+}
+
+const errHandler = (err, req, res, next) => {
+  /* if the error in development then send stack trace to display whole error,
+  if it's in production then just send error message  */
+  if(process.env.NODE_ENV === 'production') {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍, ${err.stack} `);
+};
+
+const app = express();
+
+const port = process.env.PORT;
+//configure body-parser
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+//session middleware
+app.use(session({
+  secret: 'ilikecake',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(express.static('public'));
+// initialise passport​
+app.use(passport.initialize());
+// Add passport.authenticate(..)  to middleware stack for protected routes​
+app.use('/api/movies', passport.authenticate('jwt', {session: false}), moviesRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/genres', genresRouter)
+
+app.use(errHandler);
+
+app.listen(port, () => {
+  console.info(`Server running at ${port}`);
+});
